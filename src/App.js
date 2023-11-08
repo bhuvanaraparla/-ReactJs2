a3bd257010250743966d423cb333d73a8bcc673b
bhuvanaraparla <bhuvanaraparla7677@gmail.com>
2023-11-08 09:44:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const handleButtonClick = (value) => {
+    if (value === "=") {
+      try {
+        setResult(eval(expression));
+      } catch (error) {
+        setResult("Error");
+      }
+    } else if (value === "C") {
+      setExpression("");
+      setResult("");
+    } else {
+      setExpression(expression + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`calculator ${theme}`}>
+      <h1>React Calculator</h1>
+      <div className="theme-toggle" onClick={toggleTheme}>
+        {theme === "light" ? "🌙" : "☀️"}
+      </div>
+      <div className="display">
+        <div className="expression">{expression}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="keypad">
+        <button onClick={() => handleButtonClick("7")}>7</button>
+        <button onClick={() => handleButtonClick("8")}>8</button>
+        <button onClick={() => handleButtonClick("9")}>9</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
+        <button onClick={() => handleButtonClick("4")}>4</button>
+        <button onClick={() => handleButtonClick("5")}>5</button>
+        <button onClick={() => handleButtonClick("6")}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
+        <button onClick={() => handleButtonClick("1")}>1</button>
+        <button onClick={() => handleButtonClick("2")}>2</button>
+        <button onClick={() => handleButtonClick("3")}>3</button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => handleButtonClick("0")}>0</button>
+        <button onClick={() => handleButtonClick(".")}>.</button>
+        <button onClick={() => handleButtonClick("=")}>=</button>
+        <button onClick={() => handleButtonClick("C")}>C</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+      </div>
     </div>
   );
 }
